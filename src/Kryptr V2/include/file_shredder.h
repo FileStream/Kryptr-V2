1923197038d870cdbd6589baf702a91e36ab2574
Sysroot <20374744+hochladen@users.noreply.github.com>
2020-07-07 03:08:30 -0500

    Release 2.2.4

--- src/Kryptr V2/include/file_shredder.h
+++ src/Kryptr V2/include/file_shredder.h
@@ -6,6 +6,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <filesystem>
+#include "exceptions.h"
+#include "fileapi.h"
+#include "ioapiset.h"
+#include "winioctl.h"
+#include "colors.h"
 
 using namespace std::filesystem;
 
@@ -80,17 +85,59 @@ private:
 		return true;
 	}
 
+	//Use IOCTL storage query to determine if drive is SSD
+	static bool DataOnSSD(const path& inputPath) {
+		std::string driveLetter = std::string("\\\\.\\") + inputPath.string().substr(0, 2);
+		HANDLE driveHandle = CreateFileA(driveLetter.c_str(), 0, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0); //Query the handle for the drive
+		if (driveHandle != INVALID_HANDLE_VALUE) {
+
+			STORAGE_PROPERTY_QUERY spqTrim;
+			spqTrim.PropertyId = STORAGE_PROPERTY_ID::StorageDeviceTrimProperty;
+			spqTrim.QueryType = STORAGE_QUERY_TYPE::PropertyStandardQuery;
+
+			DEVICE_TRIM_DESCRIPTOR dtd = { 0 };
+			DWORD bytesReturned = 0;
+			DeviceIoControl(
+				driveHandle,                 // handle to device
+				IOCTL_STORAGE_QUERY_PROPERTY, // dwIoControlCode
+				&spqTrim,                             // lpInBuffer
+				sizeof(spqTrim),                                // nInBufferSize
+				&dtd,             // output buffer
+				sizeof(DEVICE_TRIM_DESCRIPTOR),           // size of output buffer
+				&bytesReturned,        // number of bytes returned
+				0       // OVERLAPPED structure
+			);
+
+			//Dealloc
+			CloseHandle(driveHandle);
+
+			return dtd.TrimEnabled;
+		}
+		else {
+			throw KV2Exception("Unable to obtain drive handle.");
+			std::cin.ignore(LLONG_MAX);
+			return false;
+		}
+	}
+
 public:
 
 	// Shred a file
 	static void ShredFile(const path& inputPath)
 	{
-		if (!WriteRandomData(inputPath))
+		if (DataOnSSD(inputPath)) {
+			Colors::TextColor(YELLOW, BLACK);
+			printf("\n\nFile at \"%s\" is on an SSD, and thus cannot be shredded.\nIf you would like to securely erase this file, \
+it is recommended that you either wipe the drive or execute a trim.", inputPath.string().c_str());
+			Colors::TextColor(WHITE, BLACK);
+		}
+		else if (!WriteRandomData(inputPath))
 		{
-			printf("\n\nFILE AT \"%s\" FAILED TO SHRED. USER IS ADVISED TO RESOLVE.", inputPath.string().c_str());
+			Colors::TextColor(YELLOW, BLACK);
+			printf("\n\nFILE AT \"%s\" FAILED TO SHRED.", inputPath.string().c_str());
+			Colors::TextColor(WHITE, BLACK);
 			return;
 		}
-
 		remove(inputPath);
 	}
 
