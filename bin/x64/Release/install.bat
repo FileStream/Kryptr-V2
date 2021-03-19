9c0aa471b64672dc746087b3bf2db5d613dfb770
Sysroot <20374744+hochladen@users.noreply.github.com>
2021-03-19 15:27:32 -0500

    Release 3.2

--- bin/x64/Release/install.bat
+++ bin/x64/Release/install.bat
@@ -1,8 +1,22 @@
 @echo off
-assoc .kv2=kryptrfile
-ftype kryptrfile="%~dp0Kryptr V2.exe" "%%1"
-cls
+SET /P INSTALLPATH=Enter an installation path, or leave blank to install in ProgramFiles: 
+IF NOT DEFINED INSTALLPATH (
+set INSTALLPATH = %PROGRAMFILES%
+)
+IF NOT EXIST "%INSTALLPATH%\Kryptr V2" (
+mkdir "%INSTALLPATH%\Kryptr V2"
+)
+IF NOT EXIST "%INSTALLPATH%\Kryptr V2" (
+echo Failed to create directory at install location.
 echo[
-echo   Installation successful
+pause
+exit
+)
+copy "%~dp0" "%INSTALLPATH%\Kryptr V2"
+cd "%PROGRAMFILES%\Kryptr V2"
+assoc .kv2=kryptrfile
+ftype kryptrfile="%INSTALLPATH%\Kryptr V2\KryptrGUI.exe" "%%1"
+cls
+echo Done.
 echo[
 pause
